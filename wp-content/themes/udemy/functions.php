9d2cc283793ddf69a2efcffb3d516730c3c364da
Ankit Verma <ankitverma3103@gmail.com>
2019-08-10 15:14:23 +0530

    add menu support

--- wp-content/themes/udemy/functions.php
+++ wp-content/themes/udemy/functions.php
@@ -4,9 +4,15 @@
 define('AN_DEV_MODE', true);
 
 // Includes
+
+// header footer imports(html, css, js)
 include( get_theme_file_path( '/includes/client/enqueue.php' ) );
+// theme setup
+include( get_theme_file_path( '/includes/setup.php' ) );
+
 
 // Hooks
 add_action( 'wp_enqueue_scripts', 'an_enqueue' );
+add_action('after_setup_theme', 'an_setup_theme');
 
 // Shortcodes
