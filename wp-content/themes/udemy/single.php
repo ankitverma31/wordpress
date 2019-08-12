654394ad775e08cd2e43e4337d4e8b8d3708f68c
Ankit Verma <ankitverma3103@gmail.com>
2019-08-12 17:14:47 +0530

    add author bio

--- wp-content/themes/udemy/single.php
+++ wp-content/themes/udemy/single.php
@@ -33,7 +33,7 @@
                 <ul class="entry-meta clearfix">
                   <li><i class="icon-calendar3"></i><?php echo get_the_date(); ?></li>
                   <li>
-                    <a href="<?php author_URL; ?>">
+                    <a href="<?php echo $author_URL; ?>">
                       <i class="icon-user"></i><?php the_author(); ?>
                     </a>
                   </li>
@@ -95,20 +95,14 @@
                 <div class="card-header">
                   <strong>
                     Posted by
-                    <a href="#">John Doe</a>
+                    <a href="<?php echo $author_URL; ?>"><?php the_author(); ?></a>
                   </strong>
                 </div>
                 <div class="card-body">
                   <div class="author-image">
-                    <img src="images/author/1.jpg" class="rounded-circle">
+                  <?php echo get_avatar( $author_ID, 90, '', false, ['class'=> 'img-circle'] ); ?>
                   </div>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  Dolores, eveniet, eligendi et nobis neque minus mollitia sit
-                  repudiandae ad repellendus recusandae blanditiis praesentium v
-                  itae ab sint earum voluptate velit beatae alias fugit
-                  accusantium laboriosam nisi reiciendis deleniti tenetur
-                  molestiae maxime id quaerat consequatur fugiat aliquam
-                  laborum nam aliquid. Consectetur, perferendis?
+                  <?php echo nl2br(get_the_author_meta('description')); ?>
                 </div>
               </div><!-- Post Single - Author End -->
 
